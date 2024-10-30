30496ac3aa3469b1a0a9c253c3c9ed4e3ad0ed06
PAVIKARS <pavikars1907@gmail.com>
2024-10-30 10:34:00 +0530

    Created Auth Components login,SignUp,Logout.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,7 +10,7 @@ import { RouterModule } from '@angular/router';
   <h1 class="visually-hidden">Heroes examples</h1>
 
   <div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <img class="d-block mx-auto mb-4" src ="https://www.vecteezy.com/vector-art/5286750-restaurant-point-vector-food-logo" alt="" width="72" height="57">
     <h1 class="display-5 fw-bold text-body-emphasis">Go Food App</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">
