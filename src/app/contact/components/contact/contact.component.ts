2d24bcdc976c0ee2d00b8a06958733cca9d66045
PAVIKARS <pavikars1907@gmail.com>
2024-11-04 13:18:02 +0530

    Updated the UI of Contact.component

--- src/app/contact/components/contact/contact.component.ts
+++ src/app/contact/components/contact/contact.component.ts
@@ -4,12 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-contact',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      contact works!
-    </p>
-  `,
-  styles: ``
+  templateUrl: './contact.component.html',
+  styles: `
+  .card {
+            position: relative;
+            width: 100%;
+            border: none; /* Remove default border */
+        }
+        .card-img {
+            width: 100%; /* Make the image full width */
+            height: auto; /* Maintain aspect ratio */
+        }
+        .card-img-overlay {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%); /* Center the text */
+            text-align: center; /* Center text */
+            color: white; /* Text color */
+        }
+        .card-title {
+            font-size: 3rem; /* Adjust font size */
+        }
+  `
 })
 export class ContactComponent {
 
